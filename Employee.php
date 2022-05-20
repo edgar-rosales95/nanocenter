aa48653a0a0c6b5e729455028a08b88c17c594cf
Miguel BuenoNunez <mbuenonunez@artemis.cs.csub.edu>
2022-05-20 04:39:54 -0700

    added pics to look nice

--- Employee.php
+++ Employee.php
@@ -8,7 +8,6 @@
 			background-color:#000000;
 		}
 		.navbar {
-			position: fixed;
 			left: 0;
   			width: 100%;
   			background-color:#006600;
@@ -48,6 +47,54 @@
 			background-color:#006600;
 			padding: 15px 0;
       		}
+					#slider{
+			overflow: hidden;
+		}
+		#slider figure{
+			position: relative;
+			width:500%;
+			margin: 0;
+			left 0;
+		animation: 20s slider infinite;
+		}
+		#slider figure img{
+			float: left;
+			width: 20%;
+		}
+		@keyframes slider{
+			0% {
+				left: 0%;
+			}
+			20% {
+				left: 0%;
+			}
+			25% {
+				left: -100%;
+			}
+			45% {
+				left: -100%;
+			}
+			50% {
+				left: -200%;
+			}
+			70% {
+				left: -200%;
+			}
+			75% {
+				left: -300%;
+			}
+			95% {
+				left: -300%;
+			}
+			100% {
+				left: -400%;
+			}
+		}
+		.image-resize {
+			width: 800;
+			height: 1400;
+		}
+
 	</style>
 </head>
 <body>
@@ -60,6 +107,15 @@
 </div>
 
 <br>
+	<div id="slider">
+		<figure>
+			<img src="images/employees/peeps_listening_presentation.jpg" class="image-resize">
+			<img src="images/employees/peeps_working_laptop.jpg" class="image-resize">
+			<img src="images/employees/peeps_working_laugh.jpg" class="image-resize">
+			<img src="images/employees/peeps_working_presntation.jpg" class="image-resize">
+			<img src="images/employees/peeps_listening_presentation.jpg" class="image-resize">
+		</figure>
+	</div>
 	<script>
 	var favicon_images = [
 					'nanoCenter1.png',
